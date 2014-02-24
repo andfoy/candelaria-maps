8dcc90474c15d6860ce331934cd0839dd12afba4
unknown <ea.margffoy10@ISC312.sis.virtual.uniandes.edu.co>
2014-02-24 17:00:28 -0500

    Added localization menu.

--- src/com/defh/mapstest/MainActivity.java
+++ src/com/defh/mapstest/MainActivity.java
@@ -6,9 +6,12 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -52,7 +55,23 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		
+		//Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+		//startActivity(intent);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	        case R.id.action_settings:
+	        	Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+	    		startActivity(intent);
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);    
+	    }
 	}
 	
 	
